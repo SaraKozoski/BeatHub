1de6e91e706666296d31b78ce7a00cb1ecd00857
SaraKozoski <sarakozoski@gmail.com>
2024-10-17 16:51:57 -0300

    FEAT: Insert tentativa dois, ainda não está funcionando

--- src/App.vue
+++ src/App.vue
@@ -24,18 +24,60 @@ onMounted(() => {
 
 
 // async function insertValuesPessoa() {
-  //const { error } = await supabase.from('pessoa').insert({ name:  })}
+//const { error } = await supabase.from('pessoa').insert({ name:  })}
+
+
+//------------NÃO TA FUNCIONANDO-----------------------
+//modelo de para insert no supabase 
+
+    // create data
+    const nome = ref([])
+    const email = ref([])
+    const datanasc = ref([])
+
+    const createWorkout = async () => {
+
+      const { error } = await supabase.from('pessoa').insert([
+        {
+          nome: nome.value,
+          email: email.value,
+          datanasc: datanasc.value
+        }
+        
+      ])
+      pessoa.value = error 
+    } 
+    
+
+ 
+  
+  
 </script>
 
 <template>
-  <ul v-for="pessoas in pessoa" :key="pessoas.id">
+  
+<!------ TA funcionando ------ <ul v-for="pessoas in pessoa" :key="pessoas.id">
     <li>{{ pessoas.nome }}</li>
     <li>{{ pessoas.email }}</li>
     <li>{{ pessoas.datanasc }}</li>
-    <li>{{ endereço}}</li>
-  </ul>
+    <li>{{ endereço }}</li>
+  </ul>-->
 
+  <!------------FUNCIONA MAS O BOTAO NAO SALVA----------------->
+  <form @submit.prevent="createWorkout">
+    <h1>formulario</h1>
+ 
   <label for="">Nome:</label>
   <input type="text" v-model="nome" />
+
+  <label for="">Email:</label>
+  <input type="text" v-model="email">
+
+
+  <label for="">Data de Nascimento:</label>
+  <input type="date" v-model="datanasc">
+
+  <button type="submit">save</button>
+
+</form>
 </template>
- 
